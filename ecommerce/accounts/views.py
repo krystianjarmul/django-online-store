e3c472c506d367457364c3fba5f63f02a2808068
krystianjarmul <krystianjarmul.kj@gmail.com>
2020-07-24 15:07:59 +0200

    Add user registration

--- ecommerce/accounts/views.py
+++ ecommerce/accounts/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render, redirect
 
+from .forms import RegistrationForm
+
 
 def register(request):
-    return render(request, 'accounts/register.html')
+    form = RegistrationForm()
+
+    if request.method == 'POST':
+
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    context = {'form': form}
+
+    return render(request, 'accounts/register.html', context)
 
 def login(request):
     return render(request, 'accounts/login.html')
