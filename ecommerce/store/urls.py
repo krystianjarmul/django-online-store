e69d42c057fac751bbca7b034e4c959c22231a18
ejarkry <krystian.jarmul@ericsson.com>
2020-09-01 16:01:12 +0200

    Add css for store template

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('store/', views.store, name='store'),
 ]
