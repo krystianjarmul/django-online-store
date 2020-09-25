7e16f08d6bd94530bacfc2f86212cca1c69b671f
ejarkry <krystian.jarmul@ericsson.com>
2020-09-25 17:52:59 +0200

    Add logged user validation to context processor

--- ecommerce/store/context_processors.py
+++ ecommerce/store/context_processors.py
@@ -1,6 +1,10 @@
 from .models import Order
 
 def products_in_cart(request):
+    if not request.user.is_authenticated:
+        return {'products_in_cart': 0}
+
     customer = request.user.customer
     order = customer.order_set.first()
+
     return {'products_in_cart': order}
